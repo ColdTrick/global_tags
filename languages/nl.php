947a5925fedd146fd55f47e19565d9662e7780ba
Jeroen Dalsem <jdalsem@coldtrick.com>
2013-10-10 15:14:05 +0200

    added dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,9 @@
+<?php
+$language = array (
+  'global_tags:settings:tags' => 'Stel standaard tags in voor gebruik op deze site (komma gescheiden)',
+  'global_tags:group:title' => 'Groep Content Tags',
+  'global_tags:group:tags' => 'Stel standaard tags in voor gebruik bij content in deze groep (komma gescheiden)',
+  'global_tags:group:new_content_tags' => 'Stel tags in voor nieuwe content in deze groep (komma gescheiden)',
+  'global_tags:action:group:save' => 'Groep Content Tags opgeslagen',
+);
+add_translation("nl", $language);
